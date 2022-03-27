88a8d24a36e6299dd7028a609d625c5d7233da48
Sundeep <sundeepm1@gmail.com>
2022-03-27 22:48:28 +0530

    adding ASP net core identity

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SampleWebapi.Models;
 
 namespace SampleWebapi.Data
 {
-    public class ApiDbContext : DbContext
+    public class ApiDbContext : IdentityDbContext
     {
         public virtual DbSet<ItemData> Items {get;set;}
 
